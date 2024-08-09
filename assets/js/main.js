95f37d70f9a56aa2ee8d3582ce96b4a7ebb10a45
Aryan saini <aryansainidevloper@gmail.com>
2024-08-09 12:27:16 +0530

    add new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,45 @@ $('#admin_siderbarr .sidebar-link').each(function () {
       document.querySelector(`.admin_siderbarr .sidebar-link[href="${lastSegment}"]`).classList.add('active');
    }
 })
+const acceptAlert = () => {
+   Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "User Accepted",
+      showConfirmButton: false,
+      timer: 1500
+   });
+}
+const DeclinedAlert = () => {
+   Swal.fire({
+      title: "Give a Reason To Reject User",
+      input: "text",
+      inputAttributes: {
+         autocapitalize: "off"
+      },
+      showCancelButton: true,
+      confirmButtonText: "Rejected",
+      showLoaderOnConfirm: true,
+   }).then((result) => {
+      if (result.isConfirmed) {
+         Swal.fire({
+            title: `User Is Rejected`,
+            imageUrl: result.value.avatar_url
+         });
+      }
+   });
+}
+$(document).ready(function ($) {
+   $('.select-dropdown__button').click(function () {
+      $('.select-dropdown__list').removeClass('d-block');
+      $(this).siblings('.select-dropdown__list').toggleClass('d-block');
+   });
+   $(document).click(function (e) {
+      if (!$(e.target).closest('.select-dropdown').length) {
+         $('.select-dropdown__list').removeClass('d-block');
+      }
+   });
+   $('.select-dropdown__list a').click(function () {
+      $(this).closest('.select-dropdown__list').removeClass('d-block');
+   });
+});
