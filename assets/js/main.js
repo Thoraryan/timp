0ad78dae38fee194c53bbb449df5da75b16a0775
Aryan saini <aryansainidevloper@gmail.com>
2024-08-25 21:07:17 +0530

    add responsive

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function slideBarOpen() {
+    $('.mobile-header').addClass('w-100')
+    $('.mobile-header').addClass('end-0')
+
+}
+function slideBarClose() {
+    $('.mobile-header').removeClass('w-100')
+    $('.mobile-header').removeClass('end-0')
+}
